3cb3d141757f161905503e3abcb0587e88280a0d
neijunior <nei.junior@fazpay.io>
2024-09-30 00:54:23 -0300

    fix: ajustado log das requests na pagina index.html

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const logsRouter = require("./routes/logs");
 
 const app = express();
 const server = http.createServer(app);
@@ -11,17 +10,28 @@ const io = new Server(server);
 // Middleware para parsing de JSON
 app.use(express.json());
 
-// Rotas
-app.use("/logs", logsRouter);
-
 // Servir o front-end (HTML simples)
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-// WebSocket para enviar os logs em tempo real
+// Aqui, adicione um middleware para emitir o log pelo WebSocket
+app.post("/logs", (req, res) => {
+  const { type, logData } = req.body;
+  // Emita o log via WebSocket para todos os clientes conectados
+  io.emit("log", { type, logData });
+
+  res.sendStatus(200);
+});
+
+// WebSocket para enviar logs em tempo real
 io.on("connection", (socket) => {
   console.log("A user connected");
+
+  // Desconexão do WebSocket
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
 });
 
 // Iniciando o servidor
